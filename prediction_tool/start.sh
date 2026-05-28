164d8bd765bf8cb1cb4e6a7ca20cb7dcf0ea5589
VD2646 <dvishal1506@gmail.com>
2026-05-28 17:09:13 +0530

    Update start.sh

--- prediction_tool/start.sh
+++ prediction_tool/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Installing dependencies..."
-pip install -r requirements.txt --break-system-packages -q
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
 
 echo "Starting Poultry Analytics server..."
 cd backend
